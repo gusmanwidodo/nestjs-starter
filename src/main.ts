ddfc244e1478bdb8cf9e356fcebea95d37299989
Gusman Widodo <gusmanwidodo@gmail.com>
2023-12-23 15:29:17 +0800

    feat(base): add fresh nestjs

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Easywa API')
+    .setDescription(
+      'The Easywa API is whatsapp easy API for general purpose of usages.',
+    )
+    .setVersion('1.0')
+    .addTag('easywa')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
